a7b85cfbb2b4f3e8f183eb6ccae216b8c70e9028
kilowat <kadantsev.sergey@gmail.com>
2019-11-15 22:38:34 +0300

    work with youtube parser

--- youtube_scarping.py
+++ youtube_scarping.py
@@ -1,8 +1,13 @@
 from ytcc.download import Download
 
-video_id = 'zD68reVP0Ek'
+#video_id = 'zD68reVP0Ek'
 download = Download()
+#download.all_videos("UCcbbZI7w1d9MX5WuxTe3fYA")
 # Language is optional and default to "en"
 # YouTube uses "en","fr" not "en-US", "fr-FR"
-captions = download.get_captions(video_id, 'en')
-print(captions)
+#caption = download.get_captions(video_id, 'en')
+#print(caption)
+
+
+def run():
+    pass
--- ytcc/download.py
+++ ytcc/download.py
@@ -9,7 +9,7 @@ from pycaption import detect_format
 from urllib.parse import urlencode
 from ytcc.storage import Storage
 from ytcc.fake_logger import FakeLogger
-
+import lib.eng_to_ipa as ipa
 
 class Download():
     base_url = 'http://www.youtube.com/watch'
@@ -23,22 +23,73 @@ class Download():
         }
         self.opts.update(opts)
 
+    def all_videos(self, channel_id):
+        with youtube_dl.YoutubeDL() as ydl:
+            try:
+                result = []
+                url = "https://www.youtube.com/channel/"+channel_id+"/videos"
+                info = ydl.extract_info(url, download=False)
+
+                for item in info["entries"]:
+                    result.append(item["id"])
+                return result
+            except Exception as err:
+                raise DownloadException(
+                    "Unable to download image: {0}".format(str(err)))
+
     def update_opts(self, opts: dict) -> None:
         self.opts.update(opts)
 
     def get_captions(self, video_id: str, language: str = 'en') -> str:
-        result = self.get_result(video_id, language)
+        self.parse_info(video_id)
+        result = {}
+        en_text = self.__parse_caption(video_id, 'en')
+        ru_text = self.__parse_caption(video_id, 'ru')
+
+        if ru_text == "":
+            return False
+
+        result['en_text'] = en_text
+        result['ru_text'] = ru_text
+        result['ipa_text'] = self.__convert_to_ipa(en_text)
+
+        info_res = self.parse_info(video_id)
+
+        result.update(info_res)
+
+        return result
+
+    def __parse_caption(self, video_id: str, langugae : str = 'en') -> str:
+        result = self.get_result(video_id, langugae)
 
         if result != 0:
-            raise Exception(
-                'Unable to download and extract captions: {0}'.format(result))
+            return ""
 
-        storage = Storage(video_id, language)
+        storage = Storage(video_id, langugae)
         file_path = storage.get_file_path()
-        with open(file_path) as f:
-            output = self.get_captions_from_output(f.read(), language)
+        with open(file_path, encoding='UTF8') as f:
+            output = self.get_captions_from_output(f.read(), langugae)
         storage.remove_file()
+
         return output
+
+    def parse_info(self, video_id):
+        opts = {}
+        with youtube_dl.YoutubeDL(opts) as ydl:
+            try:
+                info_dict = ydl.extract_info(video_id, download=False)
+
+                result = {
+                    "code" : info_dict.get("id", None),
+                    "title": info_dict.get('title', None),
+                    "thumbnail": info_dict.get('thumbnail', None),
+                    "description": info_dict.get('description', None)
+                }
+                return result
+
+            except Exception as err:
+                raise DownloadException(
+                    "Unable to download image: {0}".format(str(err)))
 
     def get_result(self, video_id: str, language: str = 'en') -> int:
         opts = self.opts
@@ -63,11 +114,29 @@ class Download():
         return '{0}?{1}'.format(self.base_url, urlencode({'v': video_id}))
 
     def get_captions_from_output(self, output: str, language: str = 'en') -> str:
-
         converter = CaptionConverter()
         converter.read(output, WebVTTReader())
         output = converter.write(SRTWriter())
         return output
+
+    def __convert_to_ipa(self, en_text):
+        list_line = en_text.split("\n")
+
+        if len(list_line) > 5:
+            result = []
+            for key, item in enumerate(list_line[:3]):
+                if key == 2:
+                    result.append(ipa.convert(item))
+                else:
+                    result.append(item)
+
+            for key, line in enumerate(list_line):
+                if key > 5 and (key - 6) % 4 == 0:
+                    result.append(ipa.convert(line))
+                elif key > 3:
+                    result.append(line)
+        return "\n".join(result)
+
 
 class DownloadException(Exception):
 
